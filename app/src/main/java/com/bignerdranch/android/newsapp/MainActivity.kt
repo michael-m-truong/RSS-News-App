0facd46f66ad19d874c98ebe2d5cdd0414485370
Michael Truong <truong.minh.michael@gmail.com>
2023-12-07 15:07:32 -0800

    remove actionbar

--- app/src/main/java/com/bignerdranch/android/newsapp/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/newsapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.bignerdranch.android.newsapp
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatDelegate
 
 
@@ -10,6 +13,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+
+        // Get the ActionBar
+        val actionBar: ActionBar? = supportActionBar
+
+        // Define ColorDrawable object and set it as the background
+        val colorDrawable = ColorDrawable(Color.TRANSPARENT)
+        actionBar?.setBackgroundDrawable(colorDrawable)
     }
 }
 
