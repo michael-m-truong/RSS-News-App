2b65a8af60baf9b0a588ec53e9a07b70fd6c7415
Michael Truong <truong.minh.michael@gmail.com>
2023-12-07 16:56:11 -0800

    change text color title actionbar to black

--- app/src/main/java/com/bignerdranch/android/newsapp/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/newsapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.bignerdranch.android.newsapp
 
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import androidx.appcompat.app.ActionBar
-import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +22,20 @@ class MainActivity : AppCompatActivity() {
         // Define ColorDrawable object and set it as the background
         val colorDrawable = ColorDrawable(Color.TRANSPARENT)
         actionBar?.setBackgroundDrawable(colorDrawable)
+
+        actionBar?.setDisplayShowTitleEnabled(true) // Ensure the title is shown
+
+        if (actionBar != null) {
+            val text: Spannable = SpannableString(actionBar.title)
+            text.setSpan(
+                ForegroundColorSpan(Color.BLACK),
+                0,
+                text.length,
+                Spannable.SPAN_INCLUSIVE_INCLUSIVE
+            )
+            actionBar.title = text
+        }
+
     }
 }
 
